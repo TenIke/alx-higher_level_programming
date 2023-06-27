c2c4215250f6dcbd786fa179d4b694a2d3d0a61d
TenIke <anthonydzani@gmail.com>
2023-06-27 12:00:21 -0700

    Python - Classes and Objects

---
+++ 0x06-python-classes/1-square.py
@@ -0,0 +1,10 @@
+#!/usr/bin/python3
+
+""" Define a class square """
+
+class Square:
+    """ Initialize a new Square.
+    Args:
+        size (int): The size of the new square.
+        """
+        self.__size = size
