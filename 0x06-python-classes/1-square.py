c65975a8299aee48f2bdf54bb429d7a60a59da51
TenIke <anthonydzani@gmail.com>
2023-06-27 12:26:40 -0700

    Python - Classes and Objects

--- 0x06-python-classes/1-square.py
+++ 0x06-python-classes/1-square.py
@@ -6,7 +6,7 @@ class Square:
     """ Represents a Square """
 
     def __init__(self, size):
-        """ Initialize a new Square.
+        """Initialize a new Square.
 
         Args:
             size (int):The size of the new square.
