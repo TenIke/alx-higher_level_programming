a9514a03cbc92362427dcec7aa3606befd76ca16
TenIke <anthonydzani@gmail.com>
2023-06-27 12:09:56 -0700

    Python - Classes and Objects

--- 0x06-python-classes/1-square.py
+++ 0x06-python-classes/1-square.py
@@ -3,8 +3,11 @@
 """ Define a class square """
 
 class Square:
-    """ Initialize a new Square.
-    Args:
-        size (int): The size of the new square.
+    """ Represents a Square """
+
+    def __init__(self, size):
+        """ Initialize a new Square.
+        Args:
+            size (int): The size of the new square.
         """
         self.__size = size
--- 0x06-python-classes/2-square.py
+++ 0x06-python-classes/2-square.py
@@ -2,12 +2,16 @@
 """ Define a class Square. """
 
 class Square:
-    """ Represents a square.
-    Args:
-        size (int): The size of the new square.
-    """
-    if not isinstance(size, int):
-        raise TypeError("size must be an integer")
-    elif size < 0:
-        raise ValueError("size must be >= 0")
-    self.__size = size
+    """ Represents a square. """
+
+    def __init__(self, size=0):
+        """ Initialize a New Square.
+
+        Args:
+            size (int): The size of the new square.
+        """
+        if not isinstance(size, int):
+            raise TypeError("size must be an integer")
+        elif size < 0:
+            raise ValueError("size must be >= 0")
+         self.__size = size
