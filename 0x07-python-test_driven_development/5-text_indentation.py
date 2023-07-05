26fcab0e0649b0902a5f60226030e534ff34eb4b
TenIke <anthonydzani@gmail.com>
2023-07-05 10:28:24 -0700

    updates

--- 0x07-python-test_driven_development/5-text_indentation.py
+++ 0x07-python-test_driven_development/5-text_indentation.py
@@ -1,19 +1,18 @@
 #!/usr/bin/python3
-""" Define the function """
+"""Define the function"""
 
 
 def text_indentation(text):
-    """ print test with two new line if the ., ?, : exist
+    """print text with two new line if the ., ?, : exist
 
     args:
-        text
+        text(string): The text to print.
     Raises:
         TypeError: if text is not a string
     """
     if not isinstance(text, str):
         raise TypeError("text must be a string")
 
-    output = ""
     i = 0
     while i < len(text) and text[i] == '':
         i += 1
